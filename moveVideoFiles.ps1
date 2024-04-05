94c642d596d3b73c0b82df3c735f5f43e4a5fe1e
deleriouseel <kristin@northcountrychapel.com>
2024-04-05 11:20:58 -0700

    check for empty dirs

--- moveVideoFiles.ps1
+++ moveVideoFiles.ps1
@@ -1,7 +1,7 @@
 $sourcePath = "D:\Studies"
-$destinationPath = "\\backup\video"
+$destinationPath = "\\DocuSynology\Videos"
 
-$minAgeInDays = 7
+$minAgeInDays = 14
 $minAgeDate = (Get-Date).AddDays(-$minAgeInDays)
 
 $logFolderPath = [Environment]::GetFolderPath("MyDocuments")
@@ -19,16 +19,31 @@ Write-Log "Script started."
 
 # Move files
 Get-ChildItem -Path $sourcePath -Recurse | Where-Object { $_.LastWriteTime -lt $minAgeDate -and $_.Extension -ne ".mkv" } | ForEach-Object {
-    $destinationFile = Join-Path -Path $destinationPath -ChildPath $_.FullName.Substring($sourcePath.Length + 1)
-    $destinationFolder = Split-Path -Path $destinationFile -Parent
+    if (-not $_.PSIsContainer) {
+        $destinationFile = Join-Path -Path $destinationPath -ChildPath $_.FullName.Substring($sourcePath.Length + 1)
+        $destinationFolder = Split-Path -Path $destinationFile -Parent
 
-    # Create destination folder if it doesn't exist
-    if (-not (Test-Path -Path $destinationFolder)) {
-        New-Item -ItemType Directory -Path $destinationFolder | Out-Null
+        # Create destination folder if it doesn't exist
+        Create-DestinationFolder -Path $destinationFolder
+
+        Move-Item -Path $_.FullName -Destination $destinationFile -Force -WhatIf
+        Write-Log "Moved $($_.FullName) to $destinationFile"
     }
-
-    Move-Item -Path $_.FullName -Destination $destinationFile -Force -WhatIf
-    Write-Log "Moved $($_.FullName) to $destinationFile"
+    else {
+        # Ignore local folder if empty
+        $localFolderPath = $destinationPath + $_.FullName.Substring($sourcePath.Length)
+        if (Folder-IsEmpty -Path $localFolderPath) {
+            Write-Log "Ignoring local empty folder: $localFolderPath"
+        }
+        else {
+            # Creat remote folder if necessary
+            $remoteFolderPath = $destinationPath + $_.FullName.Substring($sourcePath.Length)
+            if (-not (Test-Path -Path $remoteFolderPath)) {
+                Write-Log "Moving files into new remote folder: $remoteFolderPath"
+                Create-DestinationFolder -Path $remoteFolderPath
+            }
+        }
+    }
 }
 
 Write-Log "Script completed."
