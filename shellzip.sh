399e898d8d04b3dbdb6b3f5b38880c92c672adb6
deleriouseel <kristin@northcountrychapel.com>
2024-05-15 08:55:02 -0700

    used paths, logging, safety features

--- shellzip.sh
+++ shellzip.sh
@@ -1,26 +1,26 @@
 #!/bin/bash
 
-#move to correct folder
-cd ~/public_html/media/
+# Path to the media folder
+MEDIA_FOLDER="/home1/northdi7/public_html/media"
 
-#loop through names.txt
-for bookname in $(cat names.txt);
-
+# Loop through names.txt
+while IFS= read -r bookname || [ -n "$bookname" ];
 do
-#delete old folder
-#rm $bookname.zip
+    #Delete old zip folder
+    rm "$MEDIA_FOLDER/$bookname.zip"
 
-#change to folder with mp3s in it 
-cd ~/public_html/media/rafiles
+    # Append newest file to zip
+    zip -FS "$MEDIA_FOLDER/$bookname.zip" "$MEDIA_FOLDER/rafiles/$bookname"* "$(basename "$MEDIA_FOLDER/rafiles")" >> "$MEDIA_FOLDER/zip_log.txt" 2>&1 || { echo "Error: Unable to zip files."; exit 1; }
 
-#appends newest file to zip
-zip -FS ../$bookname.zip ./$bookname* $(basename $PWD)
+    # Log the number of files in rafiles
+    num_files_rafiles=$(find "$MEDIA_FOLDER/rafiles" -maxdepth 1 -name "$bookname*" | wc -l)
+    echo "Audio files: $num_files_rafiles" >> "$MEDIA_FOLDER/zip_log.txt"
 
-done
+    # Log the number of files in zip
+    # Should be +3 of actual file count
+    num_files_zip=$(zipinfo -l "$MEDIA_FOLDER/$bookname.zip" | wc -l)
+    echo "Zipped files: $num_files_zip" >> "$MEDIA_FOLDER/zip_log.txt"
 
-#how many files in rafiles
-#ls | grep $bookname | wc -l
+done < "$MEDIA_FOLDER/names.txt"
 
-#how many files in zip
-#zipinfo -l ../$bookname.zip | wc -l  
-#this will be 3 more than ls
+exit 0
