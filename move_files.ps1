10f2ff7c758d88c8bb2d4bad8a777fa788a2ed2e
deleriouseel <kristin@northcountrychapel.com>
2025-11-16 08:18:02 -0800

    Initial commit
    move files weekly

---
+++ move_files.ps1
@@ -0,0 +1,1 @@
+get-childitem -path "C:\Users\NPC\Desktop\Encoded\" -exclude *.png, 67-QAA* | where-object {$_.LastWriteTime -lt (get-date).AddDays(-14)} | move-item -destination "C:\Users\NPC\Desktop\backup\"
