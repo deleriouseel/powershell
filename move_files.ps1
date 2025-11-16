07071171d4daa89ac0d02a35574aed1ebf90b3e0
deleriouseel <kristin@northcountrychapel.com>
2025-11-16 08:20:45 -0800

    Comments
    Added a comment explaining the script's purpose.

--- move_files.ps1
+++ move_files.ps1
@@ -1,1 +1,3 @@
+# ROLAND-PC moves >2 week old files from Desktop/encoded to Desktop/backup weekly.
+
 get-childitem -path "C:\Users\NPC\Desktop\Encoded\" -exclude *.png, 67-QAA* | where-object {$_.LastWriteTime -lt (get-date).AddDays(-14)} | move-item -destination "C:\Users\NPC\Desktop\backup\"
